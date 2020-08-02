7a6c1aa53daf8e15f5e1e53399ec744985453404
renee.llup <renee.llup@gmail.com>
2020-08-02 11:30:01 +0800

    For trial

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const net				= require('net');
+const net = require('net');
 const gt06 = require('./gt06n.js');
 
-const server = net.createServer((connection) => {
+const server = net
+  .createServer((connection) => {
+    console.log('client connected');
     connection.setEncoding('hex');
     connection.on('data', (data) => {
-        let result = gt06.parse(data);
-
+      let result = gt06.parse(data);
     });
-}).listen(8090);
 
+    connection.on('end', () => {
+      console.log('client disconnected');
+    });
+  })
+  .listen(5023, () => {
+    console.log(`Server running`);
+  });
